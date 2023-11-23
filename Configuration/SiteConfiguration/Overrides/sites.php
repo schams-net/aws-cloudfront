36fb4242ceadcb26dff33843f7c018dabb089dec
Michael Schams <noreply@example.com>
2023-11-23 20:51:56 +1100

    [TASK] Optimize visual appearance of Site Configuration

--- Configuration/SiteConfiguration/Overrides/sites.php
+++ Configuration/SiteConfiguration/Overrides/sites.php
@@ -7,50 +7,52 @@
 
 $languageFile = 'EXT:aws_cloudfront/Resources/Private/Language/siteconfiguration.xlf';
 
-$newColumns = [
-    'cloudfrontActive' => [
-        'label' => 'LLL:' . $languageFile . ':cloudfrontActive.label',
-        'description' => 'LLL:' . $languageFile . ':cloudfrontActive.description',
-        'onChange' => 'reload',
-        'config' => [
-            'type' => 'check',
-        ],
-    ],
-    'cloudfrontDistributionId' => [
-        'label' => 'LLL:' . $languageFile . ':cloudfrontDistributionId.label',
-        'description' => 'LLL:' . $languageFile . ':cloudfrontDistributionId.description',
-        'displayCond' => 'FIELD:cloudfrontActive:=:1',
-        'config' => [
-            'type' => 'input',
-            'eval' => '',
-        ],
-    ],
-    'cloudfrontAccessKeyId' => [
-        'label' => 'LLL:' . $languageFile . ':cloudfrontAccessKeyId.label',
-        'description' => 'LLL:' . $languageFile . ':cloudfrontAccessKeyId.description',
-        'displayCond' => 'FIELD:cloudfrontActive:=:1',
-        'config' => [
-            'type' => 'input',
-            'eval' => '',
-        ],
-    ],
-    'cloudfrontSecretAccessKeyId' => [
-        'label' => 'LLL:' . $languageFile . ':cloudfrontSecretAccessKeyId.label',
-        'description' => 'LLL:' . $languageFile . ':cloudfrontSecretAccessKeyId.description',
-        'displayCond' => 'FIELD:cloudfrontActive:=:1',
-        'config' => [
-            'type' => 'input',
-            'eval' => '',
-        ],
+$GLOBALS['SiteConfiguration']['site']['columns']['cloudfrontActive'] = [
+    'label' => 'LLL:' . $languageFile . ':cloudfrontActive.label',
+    'description' => 'LLL:' . $languageFile . ':cloudfrontActive.description',
+    'onChange' => 'reload',
+    'config' => [
+        'type' => 'check',
     ]
 ];
+$GLOBALS['SiteConfiguration']['site']['columns']['cloudfrontDistributionId'] = [
+    'label' => 'LLL:' . $languageFile . ':cloudfrontDistributionId.label',
+    'description' => 'LLL:' . $languageFile . ':cloudfrontDistributionId.description',
+    'displayCond' => 'FIELD:cloudfrontActive:=:1',
+    'config'=> [
+        'type' => 'input',
+        'eval' => '',
+    ],
+];
+$GLOBALS['SiteConfiguration']['site']['columns']['cloudfrontAccessKeyId'] = [
+    'label' => 'LLL:' . $languageFile . ':cloudfrontAccessKeyId.label',
+    'description' => 'LLL:' . $languageFile . ':cloudfrontAccessKeyId.description',
+    'displayCond' => 'FIELD:cloudfrontActive:=:1',
+    'config' => [
+        'type' => 'input',
+        'eval' => '',
+    ],
+];
+$GLOBALS['SiteConfiguration']['site']['columns']['cloudfrontSecretAccessKeyId'] = [
+    'label' => 'LLL:' . $languageFile . ':cloudfrontSecretAccessKeyId.label',
+    'description' => 'LLL:' . $languageFile . ':cloudfrontSecretAccessKeyId.description',
+    'displayCond' => 'FIELD:cloudfrontActive:=:1',
+    'config' => [
+        'type' => 'input',
+        'eval' => '',
+    ],
+];
 
-$GLOBALS['SiteConfiguration']['site']['columns'] = array_merge_recursive(
-    $GLOBALS['SiteConfiguration']['site']['columns'],
-    $newColumns
-);
+$GLOBALS['SiteConfiguration']['site']['palettes']['group1']['showitem'] = 'cloudfrontActive';
+//$GLOBALS['SiteConfiguration']['site']['palettes']['group2']['showitem'] = 'cloudfrontDistributionId,--linebreak--,cloudfrontAccessKeyId,cloudfrontSecretAccessKeyId';
+$GLOBALS['SiteConfiguration']['site']['palettes']['group2']['showitem'] = 'cloudfrontDistributionId,cloudfrontAccessKeyId,cloudfrontSecretAccessKeyId';
 
-$tabLabel = 'LLL:' . $languageFile . ':cloudfront.tab';
+$tabLabel = 'LLL:' . $languageFile . ':cloudfront.tab.label';
+$paletteGroup1Label = 'LLL:' . $languageFile . ':cloudfront.palette.group1.label';
+$paletteGroup2Label = 'LLL:' . $languageFile . ':cloudfront.palette.group2.label';
 
-//$GLOBALS['SiteConfiguration']['site']['types']['0']['showitem'] .= ',--div--;Maintenance,cloudfrontActive,cloudfrontDistributionId,cloudfrontAccessKeyId,cloudfrontSecretAccessKeyId';
-$GLOBALS['SiteConfiguration']['site']['types']['0']['showitem'] .= ',--div--;' . $tabLabel . ',' . implode(',', array_keys($newColumns));
+$GLOBALS['SiteConfiguration']['site']['types']['0']['showitem'] .= ',' . implode(',', [
+    '--div--;' . $tabLabel,
+    '--palette--;' . $paletteGroup1Label . ';group1',
+    '--palette--;' . $paletteGroup2Label . ';group2'
+]);
